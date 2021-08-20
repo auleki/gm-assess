2e852ce20d2128f8380cf084f0368dacb36aa188
auleki <kodagiwa@gmail.com>
2021-08-20 09:04:21 +0100

    setting up navbar

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<template>
+  <div>
+    <h2>HEADER HERE</h2>
+  </div>
+</template>
+
+
+<style scoped>
+</style>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  name: "TopNav",
+};
+</script>
+
+<template>
+  <nav>
+    <div class="logo">
+      <h1>STAR WARS</h1>
+    </div>
+    <div class="nav__links">
+      <router-link to="/">STARSHIPS</router-link>
+      <router-link to="/about">CHARACTERS</router-link>
+    </div>
+  </nav>
+</template>
+
+
+
+<style scoped lang="scss">
+// use icons for links
+nav {
+  height: 100%;
+  width: 100%;
+  background: #1a1a1a;
+  padding: 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 2em;
+
+  .nav__links {
+    display: flex;
+    gap: 1em;
+  }
+
+  a {
+    text-decoration: none;
+    color: #fff;
+    &.router-link-exact-active {
+      color: crimson;
+    }
+  }
+}
+</style>
